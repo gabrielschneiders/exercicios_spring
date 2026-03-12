c76c0b675a9e5a70ab0547dba3f8daa5afd0309f
gabriel Schneiders <gabrielcauaschneiders@gmail.com>
2026-03-12 19:33:53 -0300

    Criando nossa primeira API de exemplos

---
+++ Exemplo01/src/main/java/br/com/exercicios/spring/exemplo01/service/Exemplo01Service.java
@@ -0,0 +1,10 @@
+package br.com.exercicios.spring.exemplo01.service;
+
+@org.springframework.stereotype.Service
+
+public class Exemplo01Service {
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+}
